67b56bc8ec2bea39caf00ba74a999bebd45b052a
Dongze He <32473855+DongzeHE@users.noreply.github.com>
2022-04-27 19:43:23 -0400

    rewrite as class methods

--- src/pyroe/__init__.py
+++ src/pyroe/__init__.py
@@ -1,7 +1,7 @@
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from pyroe.load_fry import load_fry
 from pyroe.make_splici_txome import make_splici_txome
 from pyroe.fetch_processed_quant import fetch_processed_quant
 from pyroe.load_processed_quant import load_processed_quant
-# from pyroe.pyroe_utils import say
+from pyroe.ProcessedQuant import ProcessedQuant
