42844f66e56716cf1df6f1984af7accbd24e979d
Rob Patro <rob-p@users.noreply.github.com>
2022-06-13 17:04:27 -0400

    Update conf.py
    ci skip

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ author = "Dongze He, Rob Patro"
 # The full version, including alpha/beta/rc tags
 release = "0.5.0"
 
-master_doc = "index"
+master_doc = "contents"
 
 # -- General configuration ---------------------------------------------------
 
@@ -54,4 +54,4 @@ html_logo = "../logo.png"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
