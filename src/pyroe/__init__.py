78d3ea850904eacb29c007c6113d8e17b16e1ac5
Rob Patro <rob@newton>
2022-06-13 00:05:16 -0400

    started implementation of convert function

--- src/pyroe/__init__.py
+++ src/pyroe/__init__.py
@@ -1,9 +1,10 @@
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 from pyroe.load_fry import load_fry
 from pyroe.make_splici_txome import make_splici_txome
 from pyroe.fetch_processed_quant import fetch_processed_quant
 from pyroe.load_processed_quant import load_processed_quant
 from pyroe.ProcessedQuant import ProcessedQuant
+from pyroe.convert import convert
 
 # flake8: noqa
