d6d63b59981c5487ebdedcede31fb94db3ad3857
DongzeHe <dongzehe.zaza@gmail.com>
2024-02-11 20:08:08 -0500

    make pyranges as optional dependency

--- src/pyroe/__init__.py
+++ src/pyroe/__init__.py
@@ -1,12 +1,19 @@
-__version__ = "0.10.0"
+__version__ = "0.9.3"
 
 from pyroe.load_fry import load_fry
-from pyroe.make_txome import make_splici_txome, make_spliceu_txome
 from pyroe.fetch_processed_quant import fetch_processed_quant
 from pyroe.load_processed_quant import load_processed_quant
 from pyroe.ProcessedQuant import ProcessedQuant
 from pyroe.convert import convert
-from pyroe.id_to_name import id_to_name
 from pyroe.pyroe_utils import output_formats
+
+
+try:
+    from pyroe.make_txome import make_splici_txome, make_spliceu_txome
+    from pyroe.id_to_name import id_to_name
+except ImportError:
+    make_splici_txome = None 
+    make_spliceu_txome = None 
+    id_to_name = None 
 
 # flake8: noqa
